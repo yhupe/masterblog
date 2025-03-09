aeb397397c64e8816efb7f14932fbf4e02ce2f41
yhupe <hannes.pickel@outlook.com>
2025-03-09 20:37:42 +0100

    adding route ('/add') and function add() and html template add.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 import os
 
@@ -15,6 +15,13 @@ def index():
 
     return render_template('index.html', posts=blogposts)
 
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        pass
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
